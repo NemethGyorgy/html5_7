47218734eee43c7d10eeefcbb4c3fef39d6926c3
unknown <html@2-03.itfactory.neta>
2015-10-01 18:41:30 +0200

    jQuery ajax teljes példa

--- js/main.js
+++ js/main.js
@@ -3,46 +3,56 @@ var az = "user_86867135";
 
 // Űrlap küldése.
 function processForm(btn) {
-    
+
+    // Gomb.
+    var btn = $(btn);
+
+    // Űrlap kiválasztása.
+    var form = btn.parents("form");
+
     // Mentjük a beviteli mezők értékeit.
     var data = {};
-    form.find( "input" ).each( function( index, input ) {
+    form.find("input").each(function (index, input) {
         data[input.name] = input.value;
     });
-    
-    // Objektum amit elküldünk a serverre.
+
+    // Objektum amit elküldünk a szerverre.
     var serverObj = {
         "user": az,
         "data": data
-    }
-    
+    };
+
     // Post indítása.
-    $.post( "http://37.139.16.100:3000", JSON.stringify(serverObj), function( respons ) {
-       console.log( respons ); 
+    $.post("http://37.139.16.100:3000", JSON.stringify(serverObj), function (respons) {
+        console.log(respons);
     });
-    
+
     console.log(serverObj);
-    
+
     return false;
 }
 
 // Adatok visszakérdezése a szerverről.
-function getData( btn ) {
-    
+function getData(btn) {
+
+    console.log(btn);
+
     // Get kérés.
-    $.getJSON( "http://37.139.16.100:3000/" + az, function( respons ) {
-       console.log( respons );
-    
-    // Űrlap kiválasztása.
-    var form = $( ".reg-form" );
-        
+    $.getJSON("http://37.139.16.100:3000/" + az, function (respons) {
+        console.log(respons);
+
+        // Űrlap kiválasztása.
+        var form = $(".reg-form");
+
         console.log(form);
-        
-        form.find( "input" ).each( function( index, input ) {
-        var name = input.name;
-            if ( respons[name]) {
+
+        form.find("input").each(function (index, input) {
+            var name = input.name;
+            if (respons[name]) {
                 input.value = respons[name];
             }
         });
+
     });
+
 }
